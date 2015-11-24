5e5b335e43bf08f2d7e27e6b44d4834170f369a1
Philippa Johnstone <pecj@hotmail.co.uk>
2015-11-24 18:46:47 +0800

    facebook oauth setup in heroku

---
+++ public/js/api.js
@@ -0,0 +1,12 @@
+var API_WRAPPER = function () {
+  this.URL_BASE = window.location.origin;
+
+  this.getSecret = function(){
+    return $.ajax({
+      url:    this.URL_BASE + "/secret",
+      method: "GET"
+    })
+  };
+};
+
+var API = new API_WRAPPER();
